52f5ac22dbb9d281e1535f67726d894c31fe0821
akashraj <akashrajkn@gmail.com>
2016-06-18 18:54:43 +0530

    check status of tri object

--- src/getLandValue.m
+++ src/getLandValue.m
@@ -5,19 +5,17 @@ function [ land_value ] = getLandValue( x, y )
 
 	%assumption - 1st column of data_set is x coordinate, 2nd column is y
 	%coordinate and 3rd column is z coordinate
-
+    
+    %chooseDataSet function yet to be implemented
 	data_set_number = chooseDataSet(x, y)
 
-    if ~exist(triangulationSetExists(data_set_number))
-    {
-
-    }
+    statusOfTriangulationObject = exist(triangulationSetExists(data_set_number), 'var');
+    
+    if statusOfTriangulationObject==1
+        tri = triangulationSet(data_set_number)
     else
-    {
-
-    	tri = delaunayTriangulation(data_set(:, 1), data_set(:, 2));
-    }
-
+        tri = delaunayTriangulation(data_set(:, 1), data_set(:, 2));
+    end
     
     land_value = triangulationDelaunay(tri, x, y);
 
